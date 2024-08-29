183f62de4807f351e5945f91f9b14ea61084259d
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-08-29 15:49:06 +0200

    Some changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [currentLocalisation, setCurrentLocalisation] = useState({});
+  const [currentLocalization, setCurrentLocalization] = useState({});
+  const [nerbayLocalisations, setNerbayLocalisations] = useState(null);
+
   useEffect(() => {
     if ("geolocation" in navigator) {
       console.log("si");
       navigator.geolocation.getCurrentPosition(showPosition);
+      getNerbayStops();
     } else {
       console.log("no");
     }
@@ -15,9 +18,18 @@ function App() {
       const longitude = position.coords.longitude;
       /* const accuracy = position.coords.accuracy; */
 
-      console.log("Latitude:", latitude);
-      console.log("Longitude:", longitude);
-      setCurrentLocalisation({ latitude: latitude, longitude: longitude });
+      setCurrentLocalization({ latitude: latitude, longitude: longitude });
+    }
+    async function getNerbayStops() {
+      try {
+        const res = await fetch(
+          "https://api.resrobot.se/v2.1/location.nearbystops?originCoordLat=59.1921152&originCoordLong=17.907712&format=json&accessId=967b1027-72b3-4502-9b8c-2f950af672f5"
+        );
+        const data = await res.json();
+        console.log(data.stopLocationOrCoordLocation);
+      } catch (error) {
+        console.error(error);
+      }
     }
   }, []);
 
@@ -25,8 +37,8 @@ function App() {
     <div className="App">
       <h1>Pendlaren App</h1>
       <h2>Your Current Position: </h2>
-      <p>Latitude = {currentLocalisation.latitude}</p>
-      <p>Longitude = {currentLocalisation.longitude}</p>
+      <p>Latitude = {currentLocalization.latitude}</p>
+      <p>Longitude = {currentLocalization.longitude}</p>
     </div>
   );
 }
